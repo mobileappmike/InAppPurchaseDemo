c7eb17e79a309826868175407b3536f3fadd19e3
Michael Miles <michael.gage.miles@gmail.com>
2019-03-08 22:24:40 -0800

    Added payment requests and ability to make in-app purchases.

--- InAppPurchaseDemo/ViewController.swift
+++ InAppPurchaseDemo/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import StoreKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SKPaymentTransactionObserver {
+    
+    let productID = "com.michaelgagemiles.InAppPurchaseDemo.PurchaseExample"
+    
+    @IBOutlet weak var purchaseLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        SKPaymentQueue.default().add(self)
+        
+        purchaseLabel.text = "Purchase not completed yet"
     }
-
+    
+    @IBAction func purchasePressed(_ sender: Any) {
+        if SKPaymentQueue.canMakePayments() {
+            let paymentRequest = SKMutablePayment()
+            paymentRequest.productIdentifier = productID
+            SKPaymentQueue.default().add(paymentRequest)
+        } else {
+            print("User unable to make payments")
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            if transaction.transactionState == .purchased {
+                //if item has been purchased
+                print("Transaction Successful")
+                purchaseLabel.text = "Purchase Completed!"
+            } else if transaction.transactionState == .failed {
+                print("Transaction Failed")
+            }
+        }
+    }
 
 }
 
